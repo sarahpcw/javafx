b37ab2dc27a9c54a42525a2f97eb27b40ce6baa0
sarahpcw <36297778+sarahpcw@users.noreply.github.com>
2025-07-07 11:27:06 +0100

    Update b_carpetVat.java

--- b_carpetVat.java
+++ b_carpetVat.java
@@ -7,13 +7,13 @@ public abstract class b_carpetVat extends b_IMethods{
 	// it runs WITHOUT a method call, it cannot be called
 	// it  is autoexecuted when i create the object
 	// it will run only once!!
-	double vatPercentage = 0.0;  // class variable , blue, all methods can use it
+	double vatPercentage = 0.00;  // class variable , blue, all methods can use it
 	public b_carpetVat (double vat, double cp, double lp){
 		super ( lp, cp);
 		this.vatPercentage = vat;
 	}
 	public double addVat (double fullprice){
-		double pricePlusVat = 0.0;
+		double pricePlusVat = 0.00;
 		pricePlusVat  = fullprice * vatPercentage ;
 		return pricePlusVat;
 	}
